47e7ffa71510cb2a3a6b03e47a49a085bbe537ec
Aleksandr Spiridonov <spiridonov.xander@gmail.com>
2023-01-18 11:37:46 +0300

    feat: 3 test incorrect

--- src/index.js
+++ src/index.js
@@ -1,8 +1,41 @@
-/* module.exports = function check(str, bracketsConfig) {
+module.exports = function check(str, bracketsConfig) {
   // your solution
-} */
+  let rezultArray = []
+  let matches = {}
+  let openBrackets = []
+  let equalBrackets = []
 
-const config1 = [['(', ')']];
+  
+  for (bracketPair of bracketsConfig) {
+    openBrackets.push(bracketPair[0])
+    bracketPair[0] === bracketPair[1] ? equalBrackets.push(bracketPair[0]) :  //openBrackets.push(bracketPair[0])
+    matches[bracketPair[0]] = bracketPair[1] 
+  }
+  
+  for(char of str) {
+    if (openBrackets.indexOf(char) === -1) {
+      let popedElement = rezultArray.pop()
+      if(char != matches[popedElement]){
+        return false
+      }
+    }else{
+      if (equalBrackets.indexOf(char) != -1) {
+        let popedElement = rezultArray.pop()
+        if (popedElement != char) {
+          rezultArray.push(popedElement)
+          rezultArray.push(char)
+        }
+      } else {
+        rezultArray.push(char)
+      }
+      
+    }
+  }
+
+  return rezultArray.length === 0
+}
+
+/* const config1 = [['(', ')']];
 const config2 = [['(', ')'], ['[', ']']];
 const config3 = [['(', ')'], ['[', ']'], ['{', '}']];
 const config4 = [['|', '|']];
@@ -11,38 +44,49 @@ const config6 = [['1', '2'], ['3', '4'], ['5', '6'], ['7', '7'], ['8', '8']];
 const config7 = [['(', ')'], ['[', ']'], ['{', '}'], ['|', '|']];
 
 const str1 = '111115611111111222288888822225577877778775555666677777777776622222'
+const str3 = '[(])'
 
 
 
 function check(str, bracketsConfig) {
 
-  rezultArray = []
+  let rezultArray = []
   let matches = {}
-  openBrackets = []
-  equalBrackets = []
+  let openBrackets = []
+  let equalBrackets = []
 
   
   for (bracketPair of bracketsConfig) {
-    bracketPair[0] === bracketPair[1] ? equalBrackets.push(bracketPair[0]) : openBrackets.push(bracketPair[0])
+    openBrackets.push(bracketPair[0])
+    bracketPair[0] === bracketPair[1] ? equalBrackets.push(bracketPair[0]) :  //openBrackets.push(bracketPair[0])
     matches[bracketPair[0]] = bracketPair[1] 
   }
   console.log(openBrackets)
+  console.log(equalBrackets)
   console.log(str)
 
   for(char of str) {
     if (openBrackets.indexOf(char) === -1) {
-      popedElement = rezultArray.pop()
+      let popedElement = rezultArray.pop()
       if(char != matches[popedElement]){
-        console.log(rezultArray, char)
+        console.log(rezultArray, popedElement, char)
         return false
       }
     }else{
-      rezultArray.push(char)
+      if (equalBrackets.indexOf(char) != -1) {
+        let popedElement = rezultArray.pop()
+        if (popedElement != char) {
+          rezultArray.push(popedElement)
+          rezultArray.push(char)
+        }
+      } else {
+        rezultArray.push(char)
+      }
+      
     }
-    /* console.log(char) */
   }
   // your solution
-  console.log(matches, openBrackets)
+  console.log(matches, openBrackets, rezultArray)
 } 
 
-check(str1, config6)
+check(str1, config6) */
